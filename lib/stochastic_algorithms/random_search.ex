5f3f160de7b8c7ab9271ea6eae516ed10d214a6e
Marcus Young <marc@marcuss-mbp-4.gateway.pace.com>
2014-12-31 08:42:56 -0800

    Use aliases

--- lib/stochastic_algorithms/random_search.ex
+++ lib/stochastic_algorithms/random_search.ex
@@ -1,9 +1,11 @@
 defmodule CleverElixirAlgorithms.StochasticAlgorithms.RandomSearch do
+  alias CleverElixirAlgorithms.Utilities, as: Utils
+
   def algorithm_2_2_1(%{ problem_size: problem_size, max_iter: mi, search_space: sample_space, cost: cost_fn }) do
     iterations = Range.new(1, mi)
-    objective = CleverElixirAlgorithms.Utilities.get_objective_fn cost_fn, fn x, y -> x + y end
-    cost_compare = CleverElixirAlgorithms.Utilities.cost_compare_fn objective
-    random_sample = CleverElixirAlgorithms.Utilities.random_sample_fn problem_size, sample_space
+    objective = Utils.get_objective_fn cost_fn, fn x, y -> x + y end
+    cost_compare = Utils.cost_compare_fn objective
+    random_sample = Utils.random_sample_fn problem_size, sample_space
 
     candidates = Enum.map iterations, fn(_) -> random_sample.() end
     best = Enum.reduce(candidates, cost_compare)
@@ -12,9 +14,9 @@ defmodule CleverElixirAlgorithms.StochasticAlgorithms.RandomSearch do
 
   def search(%{ problem_size: problem_size, max_iter: mi, search_space: sample_space, cost: cost_fn }) do
     iterations = Range.new(1, mi)
-    objective = CleverElixirAlgorithms.Utilities.get_objective_fn cost_fn, fn x, y -> x + y end
-    cost_compare = CleverElixirAlgorithms.Utilities.cost_compare_fn objective
-    random_sample = CleverElixirAlgorithms.Utilities.random_sample_fn problem_size, sample_space
+    objective = Utils.get_objective_fn cost_fn, fn x, y -> x + y end
+    cost_compare = Utils.cost_compare_fn objective
+    random_sample = Utils.random_sample_fn problem_size, sample_space
 
     Enum.reduce(iterations, random_sample.(), fn(_, acc) -> cost_compare.(random_sample.(), acc) end)
   end
--- test/utilities_test.exs
+++ test/utilities_test.exs
@@ -1,37 +1,38 @@
 defmodule UtilitiesTest do
   use ExUnit.Case
+  alias CleverElixirAlgorithms.Utilities, as: Utils
 
   test "random_uniform_value" do
-    assert CleverElixirAlgorithms.Utilities.random_uniform_value > 0
-    assert CleverElixirAlgorithms.Utilities.random_uniform_value <= 1
-    assert CleverElixirAlgorithms.Utilities.random_uniform_value(5) > 0
-    assert CleverElixirAlgorithms.Utilities.random_uniform_value(5) <= 5
-    assert CleverElixirAlgorithms.Utilities.random_uniform_value([5, 7]) > 5
-    assert CleverElixirAlgorithms.Utilities.random_uniform_value([5, 7]) <= 7
+    assert Utils.random_uniform_value > 0
+    assert Utils.random_uniform_value <= 1
+    assert Utils.random_uniform_value(5) > 0
+    assert Utils.random_uniform_value(5) <= 5
+    assert Utils.random_uniform_value([5, 7]) > 5
+    assert Utils.random_uniform_value([5, 7]) <= 7
   end
 
   test "get_objective_fn" do
-    objective = CleverElixirAlgorithms.Utilities.get_objective_fn fn x -> x*x end, fn x, y -> x + y end
+    objective = Utils.get_objective_fn fn x -> x*x end, fn x, y -> x + y end
     assert objective.([1, 2, 3, 4,]) == 30
   end
 
   test "random_sample_fn" do
-    random_sample = CleverElixirAlgorithms.Utilities.random_sample_fn 10, [5, 7]
+    random_sample = Utils.random_sample_fn 10, [5, 7]
     assert length(random_sample.()) == 10
     assert Enum.reduce(random_sample.(), true, fn(x, acc) -> acc && (x > 5) && (x <= 7) end)
 
-    random_sample = CleverElixirAlgorithms.Utilities.random_sample_fn 10, 5
+    random_sample = Utils.random_sample_fn 10, 5
     assert length(random_sample.()) == 10
     assert Enum.reduce(random_sample.(), true, fn(x, acc) -> acc && (x > 0) && (x <= 5) end)
 
-    random_sample = CleverElixirAlgorithms.Utilities.random_sample_fn 10
+    random_sample = Utils.random_sample_fn 10
     assert length(random_sample.()) == 10
     assert Enum.reduce(random_sample.(), true, fn(x, acc) -> acc && (x > 0) && (x <= 1) end)
   end
 
   test "cost_compare_fn" do
-    objective = CleverElixirAlgorithms.Utilities.get_objective_fn fn x -> x*x end, fn x, y -> x + y end
-    cost_compare = CleverElixirAlgorithms.Utilities.cost_compare_fn objective
+    objective = Utils.get_objective_fn fn x -> x*x end, fn x, y -> x + y end
+    cost_compare = Utils.cost_compare_fn objective
     assert cost_compare.([1, 2, 3, 4], [5, 6, 7, 8]) == [1, 2, 3, 4]
   end
 end
